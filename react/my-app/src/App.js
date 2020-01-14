616c0a2cf6e98d51cb66eac5ae9d41f9200913ce
Anton Engman <anton.engman@knowit.se>
2020-01-14 13:51:52 +0100

    Added react frontend view thats using the ContentDeliveryAPI to display content defined in the CMS

---
+++ react/my-app/src/App.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			StartPageID: null,
+			ImageURL: null
+		}
+	}
+	
+	componentDidMount(){
+		if(this.props.apiBase){
+			fetch(this.props.apiBase + '/site', {
+				headers:{
+					'Accept': 'application/json'
+				}
+			})
+			.then( response => response.json() )
+			.then( (responseData) =>
+			{this.setState({StartPageID : responseData[0].contentRoots.startPage.id })
+			
+				fetch(this.props.apiBase + '/content/' + this.state.StartPageID, {
+					headers:{
+						'Accept': 'application/json'
+					}
+				})
+				.then(response => response.json())
+				.then((responseData) =>
+				{this.setState({ImageURL : responseData.siteLogotype.url.value})})
+			})
+		}
+	}
+	
+	render(){
+		return(
+			<div className="App">
+			<header className="App-header">
+				<img src={'http://localhost:58645/' + this.state.ImageURL} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a>
+			</header>
+			</div>
+		);
+	}
+}
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+export default App;
